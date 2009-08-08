8b0c914520b33324fc691c96085083ea687cc81a
Jonathan <jonathan@jsl.local>
2009-08-08 16:48:20 -0400

    tweaks

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -10,7 +10,7 @@ class UserSessionsController < ApplicationController
     @user_session = UserSession.new(params[:user_session])
     if @user_session.save
       flash[:notice] = "Login successful!"
-      redirect_back_or_default usages_url
+      redirect_to usages_url
     else
       render :action => :new
     end
